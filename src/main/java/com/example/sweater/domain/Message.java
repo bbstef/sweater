f0d150c5dd3bcd6b6963b6ea1aa1c574662dcb19
bbstef <bbstef@gmail.com>
2019-02-11 14:09:23 +1000

    add login security

--- src/main/java/com/example/sweater/domain/Message.java
+++ src/main/java/com/example/sweater/domain/Message.java
@@ -9,11 +9,27 @@ import javax.persistence.Id;
 @Entity
 public class Message {
     @Id
-    @GeneratedValue(strategy= GenerationType.AUTO)
+    @GeneratedValue(strategy=GenerationType.AUTO)
     private Integer id;
 
     private String text;
     private String tag;
+
+    public Message() {
+    }
+
+    public Message(String text, String tag) {
+        this.text = text;
+        this.tag = tag;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getText() {
+        return text;
+    }
 
     public Integer getId() {
         return id;
@@ -21,14 +37,6 @@ public class Message {
 
     public void setId(Integer id) {
         this.id = id;
-    }
-
-    public String getText() {
-        return text;
-    }
-
-    public void setText(String text) {
-        this.text = text;
     }
 
     public String getTag() {
--- src/main/java/com/example/sweater/repos/MessageRepo.java
+++ src/main/java/com/example/sweater/repos/MessageRepo.java
@@ -1,4 +1,17 @@
 package com.example.sweater.repos;
 
-public interface MessageRepo {
+import com.example.sweater.domain.Message;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+// This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
+// CRUD refers Create, Read, Update, Delete
+
+@Repository
+public interface MessageRepo extends CrudRepository<Message, Long> {
+
+    List<Message> findByTag(String tag);
+
 }
